b32f57822ae469852a7c5614dd4b1538d3781f0a
Sarah McDaniel <sarah.mcdaniel@tufts.edu>
2013-05-03 02:07:09 -0400

    Working on TwiML

--- assessment/twiml.js
+++ assessment/twiml.js
@@ -2,9 +2,8 @@ var twilio = require('twilio');
 var resp = new twilio.TwimlResponse();
 
 resp.sms({
-	to:'+16037690318',
-	from:'+16034216104',
 	body:'I mean, maybe.'
 });
 
+resp.toString();
 console.log(resp.toString());
